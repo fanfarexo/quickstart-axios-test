9e380eb780913f92f91857c69ba7b838a78bbe2b
fanfarexo <fanfarexo@gmail.com>
2024-10-02 12:40:34 +0900

    promise + catch 블록

--- src/App.tsx
+++ src/App.tsx
@@ -14,32 +14,43 @@ const todoUrlPrefix = "/api/todolist_long/gdhong/";
 const requestAPI = async () => {
   let todoList: Array<TodoType> = [];
 
-  // axios
-  //   .get(listUrl)
-  //   .then((response) => {
-  //     todoList = response.data;
-  //     console.log("# TodoList : ", todoList);
-  //     return todoList[0].id;
-  //   })
-  //   .then((id) => {
-  //     return axios.get(todoUrlPrefix + id);
-  //   })
-  //   .then((response) => {
-  //     console.log("## 첫 번째 Todo : ", response.data);
-  //     return todoList[1].id;
-  //   })
-  //   .then((id) => {
-  //     axios.get(todoUrlPrefix + id).then((response) => {
-  //       console.log("## 두 번째 Todo : ", response.data);
-  //     });
-  //   });
-  let response = await axios.get(listUrl);
-  todoList = response.data;
-  console.log("# TodoList : ", todoList);
-  for (let i = 0; i < todoList.length; i++) {
-    response = await axios.get(todoUrlPrefix + todoList[i].id);
-    console.log(`# ${i + 1}번째 Todo : `, response.data);
-  }
+  axios
+    .get(listUrl)
+    .then((response) => {
+      todoList = response.data;
+      console.log("# TodoList : ", todoList);
+      return todoList[0].id;
+    })
+    .then((id) => {
+      return axios.get(todoUrlPrefix + id);
+    })
+    .then((response) => {
+      console.log("## 첫 번째 Todo : ", response.data);
+      return todoList[1].id;
+    })
+    .then((id) => {
+      axios
+        .get(todoUrlPrefix + id)
+        .then((response) => {
+          console.log("## 두 번째 Todo : ", response.data);
+        })
+        .catch((e) => {
+          if (e instanceof Error) console.log("# 에러 : ", e.message);
+          else console.log(e);
+        });
+    })
+    .catch((e) => {
+      if (e instanceof Error) console.log("# 에러 : ", e.message);
+      else console.log(e);
+    });
+
+  // let response = await axios.get(listUrl);
+  // todoList = response.data;
+  // console.log("# TodoList : ", todoList);
+  // for (let i = 0; i < todoList.length; i++) {
+  //   response = await axios.get(todoUrlPrefix + todoList[i].id);
+  //   console.log(`# ${i + 1}번째 Todo : `, response.data);
+  // }
 };
 requestAPI();
 
